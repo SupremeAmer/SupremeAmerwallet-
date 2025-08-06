c5d411f454499fe1b85ebf3c2cb9dc558151bd25
SupremeAmer <emmyplumbingservice@gmail.com>
2025-08-06 19:29:20 +0100

    Create config.ts

---
+++ app/config.ts
@@ -0,0 +1,7 @@
+import { NETWORKS } from '@constants/networks';
+
+export const DEFAULT_NETWORK = NETWORKS.ETH;
+export const APPWRITE_ENDPOINT = process.env.APPWRITE_ENDPOINT!;
+export const APPWRITE_PROJECT_ID = process.env.APPWRITE_PROJECT_ID!;
+export const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID!;
+export const TON_API_KEY = process.env.TON_API_KEY!;
