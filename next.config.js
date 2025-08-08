8450ce6b2d6337e051bfb555685a1b13bf4aac76
SupremeAmer <emmyplumbingservice@gmail.com>
2025-08-08 07:52:36 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { 
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify/browser')
+    };
+    return config;
+  }
+};
